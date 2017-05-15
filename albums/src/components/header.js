b3b310a8fc2c7c389d6ce34842304ac831778c89
gustavoleite <gustavo.jog@hotmail.com>
2017-05-15 18:22:23 -0300

    First project react-native

---
+++ albums/src/components/header.js
@@ -0,0 +1,12 @@
+//Importar bibliotecas para fazer um componente
+import React from 'react';
+import { Text } from 'react-native';
+
+
+//Fazer um componente
+const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+//Tornar um componente disponível para outras partes do App
+export default Header;
