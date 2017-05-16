c3e4aa2dc5a53ba0393d0af8f838fcea68d4446c
gustavoleite <gustavo.jog@hotmail.com>
2017-05-16 18:23:11 -0300

    Style with js

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -1,11 +1,34 @@
 //Importar bibliotecas para fazer um componente
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 
 //Fazer um componente
 const Header = () => {
-  return <Text>Albums!</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2
+  },
+
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 //Tornar um componente disponível para outras partes do App
