d939741260150399a761c184a4bca3d45c4bc9db
gustavoleite <gustavo.jog@hotmail.com>
2017-05-17 15:53:01 -0300

    Class 34, section 7 - Component Level State

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,12 +3,16 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     axios.get('http://rallycoding.herokupp.com/api/music_albums')
-    .then(response => console.log(response));
+    .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
+
     return (
       <View>
         <Text> Albums List!!!</Text>
