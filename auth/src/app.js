d3ae7739361bf2374b85a9d69feec130ea13fd1a
gustavoleite <gustavo.jog@hotmail.com>
2017-05-18 13:49:40 -0300

    Class 57, section 9 - Firebase Client Setup

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyA5GdrzARt85dULrKd0Y6aC2M1v3Kv79-g",
+      authDomain: "auth-b9892.firebaseapp.com",
+      databaseURL: "https://auth-b9892.firebaseio.com",
+      projectId: "auth-b9892",
+      storageBucket: "auth-b9892.appspot.com",
+      messagingSenderId: "402155477508"
+    });
+  }
+
   render() {
     return (
       <View>
