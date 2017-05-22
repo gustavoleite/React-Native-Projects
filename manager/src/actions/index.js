78e7f0e30cb824e7f1b100d081ac63bcdaac5373
gustavoleite <gustavo.jog@hotmail.com>
2017-05-22 18:04:11 -0300

    Class 123, section 16 - Showing a Spinner on Loading

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -3,7 +3,8 @@ import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
   LOGIN_USER_SUCESS,
-  LOGIN_USER_FAIL
+  LOGIN_USER_FAIL,
+  LOGIN_USER
 } from './types';
 
 export const emailChanged = (text) => {
@@ -22,9 +23,12 @@ export const passwordChanged = (text) => {
 
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
+    dispatch({ type: LOGIN_USER });
+
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => loginUserSucess(dispatch, user))
-      .catch(() => {
+      .catch((error) => {
+        console.log(error);
         firebase.auth().createUserWithEmailAndPassword(email, password)
           .then(user => loginUserSucess(dispatch, user))
           .catch(() => loginUserFail(dispatch));
